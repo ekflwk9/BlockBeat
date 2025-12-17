929f59eae65a9a410653fdc240242aa2dae46209
ekflwk9 <ekflwk9@naver.com>
2025-12-17 22:02:11 +0900

    FireBase 연동

---
+++ Assets/01.Scripts/Component/FireData.cs
@@ -0,0 +1,23 @@
+﻿using Firebase.Database;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireData : MonoBehaviour
+{
+    private DatabaseReference data;
+    private Dictionary<string, int> score = new(capacity: 10);
+
+    private void Start()
+    {
+        data = FirebaseDatabase.DefaultInstance.RootReference;
+        Forwarding();
+    }
+
+    private void Forwarding()
+    {
+        var userName = "user_001";
+        var child = $"users/{userName}/Score";
+
+        data.Child(child).SetValueAsync(100);
+    }
+}
