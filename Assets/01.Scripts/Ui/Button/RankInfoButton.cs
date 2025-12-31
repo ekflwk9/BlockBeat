f8b9ed6aef5318217f7c1c77991e428f088aa81c
ekflwk9 <ekflwk9@naver.com>
2026-01-01 06:56:32 +0900

    이름 입력 UI 생성

---
+++ Assets/01.Scripts/Ui/Button/RankInfoButton.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine.EventSystems;
+
+public class RankInfoButton : ButtonBase
+{
+
+#if UNITY_EDITOR
+    protected override void Reset()
+    {
+        var text = this.TryGetChildComponent<TMP_Text>();
+        if (text) text.text = "OK";
+    }
+#endif
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if (!UiManager.Get<InfoUi>().SameName())
+        {
+            base.OnPointerDown(eventData);
+            UiManager.Get<FadeUi>().FadeIn(0.3f, ChangeRankSene);
+        }
+    }
+
+    private void ChangeRankSene()
+    {
+        var newName = UiManager.Get<InfoUi>().InputName();
+        Json.SetName(newName);
+
+        FirebaseManager.AddRank();
+        SceneChangeManager.Change(SceneChangeManager.SceneName.Rank);
+    }
+}
---
+++ Assets/01.Scripts/Ui/Window/InfoUi.cs
@@ -0,0 +1,60 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoUi : UiBase
+{
+    [SerializeField] private TMP_InputField input;
+    [SerializeField] private Image buttonLine;
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        SetName<InfoUi>();
+        FindUi();
+    }
+
+    private void FindUi()
+    {
+        input = this.TryGetChildComponent<TMP_InputField>();
+        buttonLine = this.TryGetChildComponent<Image>("ButtonLine");
+    }
+#endif
+
+    private void Start()
+    {
+        InitStartLine();
+    }
+
+    private void OnDestroy()
+    {
+        buttonLine.DOKill();
+    }
+
+    private void InitStartLine()
+    {
+        var tween = buttonLine.DOFade(0f, 0.5f);
+        tween.SetLoops(-1, LoopType.Yoyo);
+        tween.SetEase(Ease.Linear);
+    }
+
+    public bool SameName()
+    {
+        var enumer = FirebaseManager.value.GetEnumerator();
+        var nickName = input.text;
+
+        while (enumer.MoveNext())
+        {
+            var key = enumer.Current.Key;
+            if (string.Equals(nickName, key)) return true;
+        }
+
+        return false;
+    }
+
+    public string InputName()
+    {
+        return input.text;
+    }
+}
