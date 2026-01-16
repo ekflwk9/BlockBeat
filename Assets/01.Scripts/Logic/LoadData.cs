255e26abaea650da7c5cdf4dc0f486173f981bcd
ekflwk9 <ekflwk9@naver.com>
2026-01-17 04:16:58 +0900

    어드레서블 매니저 생성

---
+++ Assets/01.Scripts/Logic/LoadData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class LoadData
+{
+    private List<string> loadNames = new(capacity: 50);
+    private Dictionary<SceneChangeManager.SceneName, Action> load = new();
+
+    public void Load(SceneChangeManager.SceneName _sceneName)
+    {
+        for (int i = 0; i < loadNames.Count; i++)
+        {
+            AddressablesManager.Release(loadNames[i]);
+        }
+    }
+}
