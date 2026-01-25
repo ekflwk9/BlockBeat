368b57d667b7dd5798fb95cda798ad74e5d4d7a7
ekflwk9 <ekflwk9@naver.com>
2026-01-25 18:38:31 +0900

    배경 이동 스크립트 변경

--- Assets/01.Scripts/Component/BackGround.cs
+++ Assets/01.Scripts/Component/BackGround.cs
@@ -1,29 +1,44 @@
-﻿using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BackGround : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rigid;
     [SerializeField] private bool isFront;
+    [SerializeField] private float leftPos;
+    [SerializeField] private float rightPos;
+
+    private float moveSpeed = 0.05f;
 
 #if UNITY_EDITOR
     private void Reset()
     {
+        rigid = this.RequireComponent<Rigidbody2D>();
+        rigid.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+        rigid.bodyType = RigidbodyType2D.Dynamic;
+        rigid.gravityScale = 0f;
+
         isFront = this.name.Contains("Front");
+        leftPos = this.transform.position.x + (Vector3.left.x * 0.5f);
+        rightPos = this.transform.position.x + (Vector3.right.x * 0.5f);
     }
 #endif
 
     private void Awake()
     {
-        
+        var direction = isFront ? Vector2.right : Vector2.left;
+        rigid.linearVelocity = direction * moveSpeed;
     }
 
-    private void NextMovePos()
+    private void FixedUpdate()
     {
-
+        CheckPos();
     }
 
-    private void OnDestroy()
+    private void CheckPos()
     {
-        this.transform.DOKill();
+        var thisPosX = this.transform.position.x;
+
+        if (thisPosX < leftPos) rigid.linearVelocity = Vector2.right * moveSpeed;
+        else if (rightPos < thisPosX) rigid.linearVelocity = Vector2.left * moveSpeed;
     }
 }
