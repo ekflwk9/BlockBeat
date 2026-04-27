1f0d4a670865bb44f4ed6669bab89059403ae004
ekflwk9 <ekflwk9@naver.com>
2026-04-28 00:08:22 +0900

    그라데이션 배경 추가

---
+++ Assets/01.Scripts/Component/ColorGradient.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorGradient : BaseMeshEffect
+{
+    [SerializeField] private Color topColor = Color.white;
+    [SerializeField] private Color bottomColor = Color.black;
+
+    public override void ModifyMesh(VertexHelper vh)
+    {
+        if (!IsActive()) return;
+        var vertex = new UIVertex();
+
+        for (int i = 0; i < vh.currentVertCount; i++)
+        {
+            vh.PopulateUIVertex(ref vertex, i);
+            var y = vertex.position.y;
+
+            // 위쪽일수록 topColor
+            vertex.color = 0f < y ? topColor : bottomColor;
+            vh.SetUIVertex(vertex, i);
+        }
+    }
+}
